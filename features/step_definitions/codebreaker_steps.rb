07da42a470c870f916e292fd9db8eec2f75000e6
Amar Shah <lssdamperinspector@gmail.com>
2014-12-13 12:12:54 -0500

    Define and satisfy 'Given the secret code is' step
    * Pass in secret as argument to Game#start.
    * Revise unit specs to call #start with secret.
    * Revise earlier 'When I start a new game' for #start to take argument.

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -15,15 +15,15 @@ end
 Given /^I am not yet playing$/ do end
 
 When /^I start a new game$/ do
-  Codebreaker::Game.new(out).start
+  Codebreaker::Game.new(out).start('1234')
 end
 
 Then /^I should see '([^']*)'$/ do |message|
   expect(out.messages).to include(message)
 end
 
-Given /^the secret code is '(\d+)'$/ do |arg1|
-  pending
+Given /^the secret code is '(\d{4})'$/ do |secret|
+  Codebreaker::Game.new(out).start(secret)
 end
 
 When /^I guess '(\d+)'$/ do |arg1|
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -5,14 +5,15 @@ module Codebreaker
     describe '#start' do
       let(:output) { double('Output') }
       let(:game) { Game.new(output) }
+      let(:secret) { '1234' }
       before { allow(output).to receive(:puts) }
       it 'sends a welcome message' do
         expect(output).to receive(:puts).with('Welcome to Codebreaker!')
-        game.start
+        game.start(secret)
       end
       it 'prompts for the first guess' do
         expect(output).to receive(:puts).with('Enter guess:')
-        game.start
+        game.start(secret)
       end
     end
   end
