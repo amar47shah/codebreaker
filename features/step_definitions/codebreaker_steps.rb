ae8323b3686ef5f882c31d77ce9151aaa6418926
Amar Shah <lssdamperinspector@gmail.com>
2014-12-13 10:28:18 -0500

    Add pending step definitions for features

---
+++ features/step_definitions/codebreaker_steps.rb
@@ -0,0 +1,79 @@
+Given /^I am not yet playing$/ do
+  pending
+end
+
+When /^I start a new game$/ do
+  pending
+end
+
+Then /^I should see 'Welcome to Codebreaker!'$/ do
+  pending
+end
+
+Then /^I should see 'Enter guess:'$/ do
+  pending
+end
+
+Given /^the secret code is '(\d+)'$/ do |arg1|
+  pending
+end
+
+When /^I guess '(\d+)'$/ do |arg1|
+  pending
+end
+
+Then /^the mark should be ''$/ do
+  pending
+end
+
+Then /^the mark should be '\+'$/ do
+  pending
+end
+
+Then /^the mark should be '\-'$/ do
+  pending
+end
+
+Then /^the mark should be '\+\+'$/ do
+  pending
+end
+
+Then /^the mark should be '\+\-'$/ do
+  pending
+end
+
+Then /^the mark should be '\-\-'$/ do
+  pending
+end
+
+Then /^the mark should be '\+\+\+'$/ do
+  pending
+end
+
+Then /^the mark should be '\+\+\-'$/ do
+  pending
+end
+
+Then /^the mark should be '\+\-\-'$/ do
+  pending
+end
+
+Then /^the mark should be '\-\-\-'$/ do
+  pending
+end
+
+Then /^the mark should be '\+\+\+\+'$/ do
+  pending
+end
+
+Then /^the mark should be '\+\+\+\-'$/ do
+  pending
+end
+
+Then /^the mark should be '\+\+\-\-'$/ do
+  pending
+end
+
+Then /^the mark should be '\+\-\-\-'$/ do
+  pending
+end
